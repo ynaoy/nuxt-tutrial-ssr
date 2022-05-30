63b5abe86e2bc39d1ef352bb47b8cf8400324a97
ynaoy <naozou22113@yahoo.co.jp>
2022-05-31 08:50:14 +0900

    pages以下にファイルを追加

--- src/nuxt-first/components/NavBar.vue
+++ src/nuxt-first/components/NavBar.vue
@@ -3,6 +3,9 @@
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/about" no-prefetch>About page not prefetched</nuxt-link>
+      <nuxt-link to="/about" :prefetch="false">About page not prefetched</nuxt-link>
+      <nuxt-link :to="{ name: 'users-list' }">User List</nuxt-link>
     </nav>
   </div>
 </template>
---
+++ src/nuxt-first/pages/[category]/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>カテゴリー:{{  useRoute().params.category }}</h1>
+</template>
+
+<script>
+  import { useRoute } from 'vue-router'
+  export default {
+    setup(){
+      return { useRoute } //useRoute()はnuxt2におけるthis.$route
+    }
+  }
+</script>
